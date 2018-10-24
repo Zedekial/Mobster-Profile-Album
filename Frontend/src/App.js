8b0af044cb5d6c60a1d0c044508f4293fc6e25b1
Monica Gomes <mgomes@mobiquityinc.com>
2018-10-24 16:14:57 +0200

    Created Card Component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CardComponent from './components/CardComponent';
 import './CSS/App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>The fantastic Mobster Photo Album - v.033a</h1>
+        <CardComponent src="/test" name="Irina Nicula" phone="xxx" role="xxx" email="email@xxx"></CardComponent>
       </div>
     );
   }
