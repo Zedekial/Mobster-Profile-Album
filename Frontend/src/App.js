74dde8bad6c6fd9494e501c79994fba377cf302f
David Garrood <dgarrood@mobiquityinc.com>
2018-10-24 12:56:23 +0200

    resetting file contents due to errors

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CSS/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The fantastic Mobster Photo Album - v.033a</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
