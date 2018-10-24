97678df461337b983d0739891170c20080a106f7
rtenhacken <rtenhacken@mobiquityinc.com>
2018-10-24 13:52:06 +0200

    added comp to app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './CSS/App.css';
+import HeaderComponent from './components/HeaderComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>The fantastic Mobster Photo Album - v.033a</h1>
+        <HeaderComponent />
       </div>
     );
   }
