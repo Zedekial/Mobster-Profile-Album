fc4b77005fda6cc5b606774dc47282a3a40a1845
rtenhacken <rtenhacken@mobiquityinc.com>
2018-10-24 14:10:35 +0200

    resolving merge issues

--- Frontend/src/components/HeaderComponent.js
+++ Frontend/src/components/HeaderComponent.js
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import ReactDOM from 'react';
-=======
->>>>>>> 14d711558c037980f60e837ecee3b64644bdc1cc
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -10,12 +7,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-<<<<<<< HEAD
 // import MenuIcon from "@material-ui/icons/Menu";
-=======
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
->>>>>>> 14d711558c037980f60e837ecee3b64644bdc1cc
 
 const styles = {
   root: {
@@ -36,21 +28,9 @@ function HeaderComponent(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-<<<<<<< HEAD
           <Typography variant="h6" color="inherit" className={classes.grow}>
             LOGO
           </Typography>
-=======
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Menu"
-          />
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            LOGO
-          </Typography>
-          <SearchIcon />
->>>>>>> 14d711558c037980f60e837ecee3b64644bdc1cc
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
