d1c7d97300d43aa5acb4a3b33dc2b97e43c80a81
Cezary Skura <cskura@mobiquityinc.com>
2018-10-25 10:45:00 +0200

    LoginPageComponent added, react-route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './CSS/App.css';
 import HeaderComponent from './components/HeaderComponent';
-
+import LoginPageComponent from './components/LoginPageComponent'
 class App extends Component {
   constructor(){
     super();
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <div className="App">
        <HeaderComponent />
+       <LoginPageComponent />
         <h1>The fantastic Mobster Photo Album - v.033a</h1>
       </div>
     );
