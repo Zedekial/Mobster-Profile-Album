e6823f754ccaedaa4343b6f8b6c5eb17df8bfc93
Cezary Skura <cskura@mobiquityinc.com>
2018-10-25 14:19:07 +0200

    private route added /admin

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './CSS/App.css';
 import HeaderComponent from './components/HeaderComponent';
-import LoginPage from './components/LoginPage'
+import LoginPage, { fakeAuth } from './components/LoginPage'
 import { Redirect, Link, Route, Switch } from 'react-router-dom';
 
+
+const Admin = () => ( <div> <h2>I am in GOD MODE ADMIN</h2> </div> )
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => fakeAuth.isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />} />
+  )
+}
 
 class App extends Component {
   constructor(){
@@ -21,6 +33,7 @@ class App extends Component {
        <Switch>
        <Route path="/login" component={LoginPage}/>
        <Route exact path="/" component={HeaderComponent} />
+       <PrivateRoute path='/admin' component = {Admin} />
        </Switch>
        {/* <LoginPageComponent /> */}
         
