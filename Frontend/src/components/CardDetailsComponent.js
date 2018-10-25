c0bb7c64e038fc3c7e535a06f5156a9dd2dbcec6
Monica Gomes <mgomes@mobiquityinc.com>
2018-10-25 13:11:10 +0200

    Refactored card component css stateless card componenents to use arrow functions

--- Frontend/src/components/CardDetailsComponent.js
+++ Frontend/src/components/CardDetailsComponent.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 
-export class CardDetailsComponent extends Component{
-    render() {
-        return (
-            <div onClick={this.props.onClick} className={this.props.className}>
-                <h2>{this.props.name}</h2>
-                <ul>
-                    <li>phone: {this.props.phone}</li>
-                    <li>email: {this.props.email}</li>
-                    <li>role: {this.props.role}</li>
-                </ul>
-            </div>
+export const CardDetailsComponent = (props) => {
+    return (
+        <div onClick={props.onClick} className={props.className}>
+            <h2>{props.name}</h2>
+            <ul>
+                <li>Phone: {props.phone}</li>
+                <li>Email: {props.email}</li>
+                <li>Role: {props.role}</li>
+            </ul>
+        </div>
         );
     }
-}
+    
--- Frontend/src/components/CardMediaComponent.js
+++ Frontend/src/components/CardMediaComponent.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
-export class CardMediaComponent extends Component {
-    render(){
-        return ( 
-        <div onClick={this.props.onClick} className={this.props.className}>
-            <img src={this.props.src}></img>
-            <h1>{this.props.name}</h1>
+export const CardMediaComponent = (props) => {
+    return ( 
+        <div onClick={props.onClick} className={props.className}>
+        <img src={props.src}></img>
+        <h1>{props.name}</h1>
         </div>
         );
     }
-}
