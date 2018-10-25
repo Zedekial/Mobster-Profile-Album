05e4e4db3ca1c8d4e05d1fecd4657d437d4bffbb
rtenhacken <rtenhacken@mobiquityinc.com>
2018-10-25 15:48:40 +0200

    tried some auth func

--- Frontend/src/components/LoginPage.js
+++ Frontend/src/components/LoginPage.js
@@ -12,31 +12,41 @@ class LoginPage extends Component {
     fakeAuth.authenticate(() => { this.setState({ redirectToReferrer: true }) })
     }
   render() {
-    const { from } = this.props.location.state || { from: { pathname: '/' } } 
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
     const { redirectToReferrer } = this.state;
-    
- if (redirectToReferrer) { 
-  return ( <Redirect to={from} /> ) }
+
+ if (redirectToReferrer) {
+  return ( <Redirect to={from} />  ) }
     return (
       <div>
         <h1>Login Page</h1>
         <form onSubmit={
           (e)=>e.preventDefault()
         }>
-            <input name="login" placeholder="login"></input>
+            <input id='loginField' name="login" placeholder="login"></input>
             <input name="password" placeholder="password"></input>
-            <button type="submit" onClick={
-              this.login
-            }>Login
-     </button>
+            <button type="submit" onClick={ this.login }>Login</button>
         </form>
       </div>
     )
   }
 }
+
+const readField = () => {
+  console.log('hello readField');
+}
+
 export const fakeAuth = {
-  isAuthenticated: false, authenticate(cb) { 
-    this.isAuthenticated = true 
+  isAuthenticated: false, authenticate(cb) {
+    this.isAuthenticated = true
     setTimeout(cb, 100) },
  }
  export default LoginPage;
+
+
+ // var loginName = 'admin';
+ // var input_value = document.getElementById('loginField').value;
+ // console.log(input_value);
+ //  if (input_value === loginName) {
+ //    console.log(input_value);
+ //  }
