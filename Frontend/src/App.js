47e835075f033918533a56f7ef7f3f8921cea2fc
Monica Gomes <mgomes@mobiquityinc.com>
2018-10-30 16:39:36 +0100

    Fixed routes and improved styling

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AddUserButtonComponent from './components/AddUserButtonComponent'
-import './CSS/App.css';
+import AddEditFormComponent from './components/AddEditFormComponent';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {LoggedIn: true}
+  }
   render() {
     return (
       <div className="App">
-        <AddUserButtonComponent mainState={true}/>
+      <Router>
+        <div>
+          <Link to="/add"><AddUserButtonComponent state={this.state}/></Link>
+          <Route path="/add" component={AddEditFormComponent} />
+        </div>
+      </Router>
+      
       </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
