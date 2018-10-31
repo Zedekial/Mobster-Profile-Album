820da42a46736c4c435345f7085b7a1ec8454b05
David Garrood <dgarrood@mobiquityinc.com>
2018-10-31 14:25:57 +0100

    Bringing current merged app up to date

--- Frontend/src/MainPage.js
+++ Frontend/src/MainPage.js
@@ -94,7 +94,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://api.myjson.com/bins/msk5m')
+    axios.get('/mobsters')
       .then(response => {
         this.setState({
           data: response.data,
