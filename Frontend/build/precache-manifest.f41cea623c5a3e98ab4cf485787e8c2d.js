5e85b025ae29adb5880d15138dd3c4df3f2a9e37
David Garrood <dgarrood@mobiquityinc.com>
2018-10-31 14:25:57 +0100

    Bringing current merged app up to date

---
+++ Frontend/build/precache-manifest.f41cea623c5a3e98ab4cf485787e8c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6635f62d630d4bf1a5cd",
+    "url": "/static/js/main.6635f62d.chunk.js"
+  },
+  {
+    "revision": "a56e5ae81d541d4496c1",
+    "url": "/static/js/1.a56e5ae8.chunk.js"
+  },
+  {
+    "revision": "6635f62d630d4bf1a5cd",
+    "url": "/static/css/main.b20da947.chunk.css"
+  },
+  {
+    "revision": "550d33033ac8667da300bed9bc22f3e4",
+    "url": "/index.html"
+  }
+];
