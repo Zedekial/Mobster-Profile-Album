7d1d41edf0590c933cb1ef96e8e676a9e81db1ba
Monica Gomes <mgomes@mobiquityinc.com>
2018-10-31 19:41:26 +0100

    Refactored Footer and CardComponent css

--- Frontend/src/components/FooterComponent.js
+++ Frontend/src/components/FooterComponent.js
@@ -5,12 +5,10 @@ import '../CSS/Footer.css'
 const Footer = () => {
   return (
     <footer className="footer__container">
-      <div className="footer__icon__container">
         <a href="https://www.linkedin.com/company/mobiquity-inc-europe" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="footer__icon" icon={['fab', 'linkedin']}/></a>
         <a href="https://www.instagram.com/mobiquityeu/?hl=en" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="footer__icon" icon={['fab', 'instagram']}/></a>
         <a href="https://www.facebook.com/Mobiquity/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="footer__icon" icon={['fab', 'facebook']}/></a>
         <a href="https://twitter.com/mobiquityincEU" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="footer__icon" icon={['fab', 'twitter']}/></a>
-      </div>
     </footer>
   )
 }
