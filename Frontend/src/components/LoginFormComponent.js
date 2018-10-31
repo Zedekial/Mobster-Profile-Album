b1fcfe2f26de6c29decba3712e586cefd2a6ffa6
rtenhacken <rtenhacken@mobiquityinc.com>
2018-10-31 16:23:57 +0100

    In LoginFormComponent the constructor is removed it had no function.
    In LoginPage import MainPage is removed as it is never used

--- Frontend/src/components/LoginFormComponent.js
+++ Frontend/src/components/LoginFormComponent.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../CSS/LoginPageComponent.css';
 
 class LoginFormComponent extends React.Component{
-    constructor(props){
-        super(props);
-    }
     render() {
         return(
             <div className="login__form__container">
