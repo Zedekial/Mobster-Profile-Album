103169fbf076b348d737bec5f0d2cb8e45d7b846
Irina Nicula <inicula@mobiquityinc.com>
2018-10-31 16:12:27 +0100

    Added standard css classes to MainPage.css
    Created a standard class for all buttons
    
    Created standard class for all inputs
    
    Applied standard classes to buttons and inputs in LoginFormComponent
    and AddEditFormComponent.

--- Frontend/src/components/LoginFormComponent.js
+++ Frontend/src/components/LoginFormComponent.js
@@ -10,9 +10,9 @@ class LoginFormComponent extends React.Component{
             <div className="login__form__container">
                 <h1>Authorization</h1>
                 <form className="login__form" onSubmit={this.props.onSubmit}>
-                    <input className="login__input" type="text" name="login" placeholder="login" ref="login"></input>
-                    <input className="login__input" type="password" name="password" placeholder="password" ref="password"></input>
-                    <button className="login__button" type="submit" value="submit">Login</button>
+                    <input className="standard__input__style login__input" type="text" name="login" placeholder="login" ref="login"></input>
+                    <input className="standard__input__style login__input" type="password" name="password" placeholder="password" ref="password"></input>
+                    <button className="standard__button__style login__button" type="submit" value="submit">Login</button>
                 </form>
             </div>
         )
